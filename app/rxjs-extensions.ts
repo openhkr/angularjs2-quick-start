73211e0d4855886968f5d5e1ffc4fdd17f92a758
氢氪花露水 <liushk@reachauto.com>
2017-10-10 17:35:41 +0800

    初始化上传

---
+++ app/rxjs-extensions.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/12/21.
+ */
+// Observable class extensions
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
